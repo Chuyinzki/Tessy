5c9701cc8fbda29a40273629f6889f1b15ee1d3b
Jesus Villegas <jesus_villegas@intuit.com>
2019-10-23 12:53:56 -0700

    Adding google sheets api variables

--- myconfig.py
+++ myconfig.py
@@ -4,3 +4,6 @@ client_id = "81527cff06843c8634fdc09e8ac0abefb46ac849f38fe1e431c2ef2106796384"
 client_secret = "c7257eb71a564034f9419ee651c7d0e5f7aa6bfbd18bafb5c5c033b093bb2fa3"
 email = "Get your own email"
 password = "Get your own password"
+
+sheets_client_id = "Get your own password"
+sheets_client_password = "Get your own password"
