92a2b381b0ec355662673db976dafc4952e789cc
Jesus Villegas <jesus_villegas@intuit.com>
2019-10-23 12:39:22 -0700

    Getting all vehicles, adding to dict, getting first for odometer function

--- Tessy.py
+++ Tessy.py
@@ -2,11 +2,25 @@ import requests
 import json
 from myconfig import *
 
+
+def get_vehicle_ids():
+    ret_object = {}
+    response = s.get('https://owner-api.teslamotors.com/api/1/vehicles').json()['response']
+    for obj in response:
+        ret_object[obj['id']] = obj['display_name']
+    return ret_object
+
+
+def get_vehicle_odometer(vehicle_id, vehicle_name):
+    print("Getting odometer reading for " + vehicle_name)
+    response = s.get('https://owner-api.teslamotors.com/api/1/vehicles/' + str(vehicle_id) + '/data_request/vehicle_state')
+    data = response.json()
+    return data['response']['odometer']
+
+
 s = requests.Session()
 s.headers.update({'Authorization': 'Bearer ' + access_token})
-
-response = s.get('https://owner-api.teslamotors.com/api/1/vehicles/15372580391886096/data_request/vehicle_state')
-data = response.json()
-odometer = data['response']['odometer']
+vehicles = get_vehicle_ids()
+firstID = next(iter(vehicles))
+odometer = get_vehicle_odometer(firstID, vehicles[firstID])
 print(odometer)
-
