b10a63574ca9a39ba4490139aaa549d7de8f9205
Jesus Villegas <jesus.villegas@servicenow.com>
2021-04-26 09:58:46 -0700

    Adding session get to consolidate error messaging

--- Tessy.py
+++ Tessy.py
@@ -3,11 +3,12 @@ from myconfig import *
 
 CONST_TESSY_ID = 15372580391886096
 
+
 def get_vehicle_ids(session):
     ret_object = {}
     response = {}
     try:
-        response = session.get('https://owner-api.teslamotors.com/api/1/vehicles').json()['response']
+        response = do_session_get(session, 'https://owner-api.teslamotors.com/api/1/vehicles')['response']
     except:
         print("Could not get list of vehicles. Tesla API token might be invalid.")
     for obj in response:
@@ -17,12 +18,10 @@ def get_vehicle_ids(session):
 
 def get_vehicle_odometer(vehicle_id, vehicle_name, session):
     print("Getting odometer reading for " + vehicle_name)
-    response = session.get('https://owner-api.teslamotors.com/api/1/vehicles/' + str(vehicle_id) + '/data_request/vehicle_state')
+    response = do_session_get(session,
+        'https://owner-api.teslamotors.com/api/1/vehicles/' + str(vehicle_id) + '/data_request/vehicle_state')
     data = response.json()
-    if response.status_code != 200:
-        print("Response from " + vehicle_name + " was not 200: " + str(response.status_code) + " " + response.text)
-        print("Try to wake it using your phone")
-        raise
+
     try:
         ret = data['response']['odometer']
     except TypeError as error:
@@ -32,6 +31,18 @@ def get_vehicle_odometer(vehicle_id, vehicle_name, session):
         return ret
 
 
+def do_session_get(session, url):
+    response = session.get(url)
+    print("Session get response: " + str(response.status_code))
+    if response.status_code == 401:
+        print("Authentication error. Tesla API token might be invalid.")
+        raise
+    if response.status_code != 200:
+        print("Response was not 200. Try to wake it using your phone")
+        raise
+    return response
+
+
 def get_first_odometer_reading():
     s = prepare_session()
     vehicles = get_vehicle_ids(s)
@@ -39,8 +50,10 @@ def get_first_odometer_reading():
     odometer = get_vehicle_odometer(first_id, vehicles[first_id], s)
     return odometer
 
+
 def get_tessy_odometer_reading():
     return get_vehicle_odometer(15372580391886096, "Tessy", prepare_session())
+
 
 def prepare_session():
     s = requests.Session()
